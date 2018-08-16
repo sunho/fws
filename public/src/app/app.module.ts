4855baf7e264e111a553f082e02465c9903e709c
sunho <ksunhokim123@naver.com>
2018-08-17 03:29:32 +0900

    Add Public

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
